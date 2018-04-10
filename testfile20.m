f2922cc91b5d2a4dcfe2eb1be1630e41e831d607
npatel <nirja.patel@mathworks.com>
2018-04-10 18:31:18 -0400

    adding matlab MLX

---
+++ testfile20.m
@@ -0,0 +1,20 @@
+% check file name
+% with name of func
+
+function c = divideMe(a,b)
+% divideMe
+% no help available
+
+switch nargin
+    case 2
+        c = a + b
+    case 1
+        c = a + a
+    otherwise
+        c = 0
+end
+
+
+
+
+
